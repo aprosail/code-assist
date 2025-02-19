4746075e8cd68d9fe948178faca6b7e04f409e18
James Aprosail <aprosail@outlook.com>
2025-02-19 18:12:15 +0800

    vsce package script

--- code-assist/build.ts
+++ code-assist/build.ts
@@ -1,13 +1,25 @@
 import terser from "@rollup/plugin-terser"
 import typescript from "@rollup/plugin-typescript"
-import {readFileSync, writeFileSync} from "node:fs"
+import {createVSIX} from "@vscode/vsce"
+import {copyFileSync, readFileSync, writeFileSync} from "node:fs"
 import {join, normalize, relative} from "node:path"
+import {argv} from "node:process"
 import {rollup} from "rollup"
 
 const root = import.meta.dirname
 const src = join(root, "src")
 const out = join(root, "out")
 
+/**
+ * Sync manifest (`package.json` file)
+ * from the {@link root} folder to the {@link out} folder.
+ * All its options are prepared for what a VSCode extension requires.
+ *
+ * @param root path to the root folder.
+ * @param out path to the output folder.
+ * @param outCode path to the output entry file of the extension.
+ * @param dev whether to use dev mode.
+ */
 function syncManifest(root: string, out: string, outCode: string, dev = false) {
   const filename = "package.json"
   const manifest = JSON.parse(readFileSync(join(root, filename)).toString())
@@ -24,6 +36,12 @@ function syncManifest(root: string, out: string, outCode: string, dev = false) {
   writeFileSync(join(out, filename), content)
 }
 
+/** A shortcut to copy asset files into the output folder. */
+function syncAssets(filenames: string[], root: string, out: string) {
+  for (const name of filenames) copyFileSync(join(root, name), join(out, name))
+}
+
+/** Build a ts project into an output es file. */
 async function bundle(src: string, out: string) {
   const bundle = await rollup({
     plugins: [typescript(), terser()],
@@ -45,6 +63,12 @@ async function main() {
   const outCode = join(out, `${codeName}.js`)
 
   syncManifest(root, out, outCode)
-  return bundle(srcCode, outCode)
+  await bundle(srcCode, outCode)
+
+  // Package the extension if specified by command.
+  if (argv.includes("pack")) {
+    syncAssets(["readme.md", "license.txt"], root, out)
+    await createVSIX({cwd: out})
+  }
 }
 main()
